b2877fe96e9fd4589675e2a84cb275caa4472354
hard2hannana <hard2hannana>
2025-04-21 14:03:46 -0400

    working gallery

--- src/opend_assets/src/components/Gallery.jsx
+++ src/opend_assets/src/components/Gallery.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./Item";
+import { Header} from "./Header";
+import { Principal } from "@dfinity/principal";
 
-function Gallery() {
+
+
+
+function Gallery(props) {
+
+  const [items, setItems] = useState();
+
+  function fetchNFTs() {
+    if (props.ids != undefined) {
+      setItems(
+      props.ids.map( (nftId) => (
+        <Item id={nftId} key={nftId.toText()}/>
+        ))
+      )
+    }
+  };
+ 
+  useEffect(() => {
+    fetchNFTs();
+  }, []) 
   return (
     <div className="gallery-view">
-      <h3 className="makeStyles-title-99 Typography-h3">Discover</h3>
+      <h3 className="makeStyles-title-99 Typography-h3">{props.title}</h3>
       <div className="disGrid-root disGrid-container disGrid-spacing-xs-2">
         <div className="disGrid-root disGrid-item disGrid-grid-xs-12">
-          <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center"></div>
-          <Item />
+          <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center">
+          {items}  
+          </div>        
         </div>
       </div>
     </div>
